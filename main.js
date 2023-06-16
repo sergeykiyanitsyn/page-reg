15faccc44b07a34f970b97f4225ed9610112107f
sergeykiyanitsyn <serg-kianitsyn@mail.ru>
2023-06-16 06:36:48 +0300

    заработалО

---
+++ main.js
@@ -0,0 +1,11 @@
+let userName = 'Serge';
+let userAge = 24;
+let user = null;
+console.log(userName + ' is ' + userAge + ' years old');
+console.log(0); // undefined
+console.log(user);
+console.log(`2 + 2 = ${2+2}`) // 2 + 2 = 4
+let up = Boolean('0');
+console.log(up);
+
+
