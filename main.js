552c9e469ca128cb3458d803be578ccb12a2c22b
sergeykiyanitsyn <serg-kianitsyn@mail.ru>
2024-02-22 22:32:36 +0300

    start project page reg

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-let userName = 'Serge';
-let userAge = 24;
-let user = null;
-console.log(userName + ' is ' + userAge + ' years old');
-console.log(0); // undefined
-console.log(user);
-console.log(`2 + 2 = ${2+2}`) // 2 + 2 = 4
-let up = Boolean('0');
-console.log(up);
 
+
+//function printNumbers(from, to){
+  
+  setTimeout(function printNumbers() {
+    console.log(from)
+    
+    if (from < to ) {
+      setTimeout(printNumbers, 1000);
+    }
+    from++
+  
+  }, 1000);
+//}
+
+/*function printNumbers(from, to){
+
+ let stopInterval = setInterval(() => {
+  console.log(from)
+  from++;
+   if (from < to + 1) {
+     clearInterval(stopInterval)
+   }
+
+  }, 1000);
+}*/
+
+
+//printNumbers(6,10)
+
+/*printNumbers(1,10)*/
 
